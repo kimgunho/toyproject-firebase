7c1439d53f0ad7b9b5a230c145028da05f8f8ce9
kim <yuyukgh@naver.com>
2021-12-10 23:02:46 +0900

    feat: 리스트페이지 id값 기준으로 href 연결

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
 
 import Button from '../components/Button';
 import { database } from '../firebase';
@@ -21,8 +22,12 @@ function List() {
     <div>
       <Button title={'글작성'} link={'/write'} />
       <ul>
-        {list?.map((item, index) => {
-          return <li key={index}>{item.id}</li>;
+        {list?.map((item) => {
+          return (
+            <li key={item.id}>
+              <Link to={`/detail/${item.id}`}>{item.title}</Link>
+            </li>
+          );
         })}
       </ul>
     </div>
